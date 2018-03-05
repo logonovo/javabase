6a543fa8a0b5677ae39a9acab8ad0f2aaa10fab7
logonovo <logonovo@gmail.com>
2018-03-05 22:22:32 +0800

    异步死循环

---
+++ src/main/java/com/logonovo/javabase/thread/chapter2/RunThread.java
@@ -0,0 +1,38 @@
+package com.logonovo.javabase.thread.chapter2;
+
+/**
+ * @Author 小凡
+ * Email: logonovo@gmail.com
+ * @Date 2018/3/5 22:18
+ */
+public class RunThread extends Thread {
+    private boolean isRunning = true;
+    public boolean isRunning(){
+        return isRunning;
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("进入run");
+        while(isRunning == true){
+
+        }
+        System.out.println("线程被停止了");
+    }
+
+    public static void main(String[] args) {
+        try {
+            RunThread thread = new RunThread();
+            thread.start();
+            Thread.sleep(1000);
+            thread.setRunning(false);
+            System.out.println("已经赋值为false");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
